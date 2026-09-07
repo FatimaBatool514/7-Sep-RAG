5fcf18a426442b93e590f7fb5acddaac99689ce5
FatimaBatool514 <fbatool819@gmail.com>
2026-09-07 22:52:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_classic.chains import create_retrieval_chain
-from langchain.chains import create_retrieval_chain
+from langchain_chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # Page configuration
